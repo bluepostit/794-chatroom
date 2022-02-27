c24ca332d1a37ce989f8b22f6385bd7f17307ed3
Yair <32864499+bluepostit@users.noreply.github.com>
2022-02-27 09:59:39 +0200

    chatroom functionality - no realtime

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,24 @@
+class MessagesController < ApplicationController
+  def create
+    # create a message
+    # assign a user and a chatroom
+    # save
+    # redirect to CHATROOM show page
+    @message = Message.new(message_params)
+    @message.user = current_user
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message.chatroom = @chatroom
+
+    if @message.save
+      redirect_to chatroom_path(@chatroom)
+    else
+      render 'chatrooms/show'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
